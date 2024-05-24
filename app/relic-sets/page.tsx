c1ce3fa9ceca95b5ffb0a940c5f8fac44cc9ffc6
Patricia <pkwlo1992@gmail.com>
2024-05-24 12:39:28 -0700

    load data into relic-sets page

--- app/relic-sets/page.tsx
+++ app/relic-sets/page.tsx
@@ -3,66 +3,115 @@ import path from "path";
 
 import relics from "./relics.json";
 
-function RelicCardTemplate(id: number) {
-    const relicName = relics[id].name;
-    const relic2Pc = relics[id].bonus2pc;
-    const relic4Pc = relics[id].bonus4pc;
-    const relicImage = path.join('/set-images', `${relicName.replace(/\s+/g, '_')}.png`);
+function RelicCardTemplate({
+  relicName,
+  relic2Pc,
+  relic4Pc,
+  relicImage,
+}: {
+  relicName: string;
+  relic2Pc: string;
+  relic4Pc: string;
+  relicImage: string;
+}) {
+  return (
+    <div className="flex flex-row">
+      <Image
+        src={relicImage}
+        alt={relicName}
+        width={100}
+        height={100}
+        style={{ width: "auto", height: "100%" }}
+        priority
+      />
 
-    return (
-        <><div className="flex flex-row">
-            <Image
-                src={relicImage}
-                alt={relicName}
-                width={100}
-                height={100}
-                className="flex-shrink-0"
-                priority />
-                
-                <div className="flex flex-col py-4">
-                    <h6 className="text-xl">{relicName}</h6>
-                    <p className="mt-3">2-Piece Bonus: {relic2Pc}</p>
-                    <p className="mt-3">4-Piece Bonus: {relic4Pc}</p>
-                </div>
-        </div>
-        </>
-    );
+      <div className="flex flex-col py-4">
+        <h6 className="text-xl">{relicName}</h6>
+        <p className="mt-3">2-Piece Bonus: {relic2Pc}</p>
+        {relic4Pc && <p className="mt-3">4-Piece Bonus: {relic4Pc}</p>}
+      </div>
+    </div>
+  );
 }
 
-function OrnamentCardTemplate(id: number) {
-    const ornamentName = relics[id].name;
-    const ornament2Pc = relics[id].bonus2pc;
-    const ornamentImage = path.join('/set-images', `${ornamentName.replace(/\s+/g, '_')}.png`);
+function OrnamentCardTemplate({
+  ornamentName,
+  ornament2Pc,
+  ornamentImage,
+}: {
+  ornamentName: string;
+  ornament2Pc: string;
+  ornamentImage: string;
+}) {
+  return (
+    <div className="flex flex-row">
+      <Image
+        src={ornamentImage}
+        alt={ornamentName}
+        width={100}
+        height={100}
+        style={{ width: "auto", height: "100%" }}
+        priority
+      />
 
-    return (
-        <><div className="flex flex-row">
-            <Image
-                src={ornamentImage}
-                alt={ornamentName}
-                width={100}
-                height={100}
-                className="flex-shrink-0"
-                priority />
-                
-                <div className="flex flex-col py-4">
-                    <h6 className="text-xl">{ornamentName}</h6>
-                    <p className="mt-3">2-Piece Bonus: {ornament2Pc}</p>
-                </div>
-        </div>
-        </>
-    );
+      <div className="flex flex-col py-4">
+        <h6 className="text-xl">{ornamentName}</h6>
+        <p className="mt-3">2-Piece Bonus: {ornament2Pc}</p>
+      </div>
+    </div>
+  );
 }
 
 export default function Page() {
-    return (
-        <>
-            <div className="flex flex-col items-center min-h-screen py-2">
-                <h4 className="text-3xl">Relic & Ornament Sets</h4>
-                <h4 className="text-3xl">Relics</h4>
-                <RelicCardTemplate />
-                <h4 className="text-3xl">Ornaments</h4>
-                <OrnamentCardTemplate />
-            </div>
-        </>
-    );
+  return (
+    <div className="flex flex-col items-center min-h-screen py-2">
+      <h4 className="text-3xl">Relic & Ornament Sets</h4>
+
+      <h4 className="text-3xl">Relics</h4>
+      {relics.map((relic, index) => {
+        if (relic.type === "Relic Set") {
+          const relicName = relic.name;
+          const relic2Pc = relic.bonus2pc || "";
+          const relic4Pc = relic.bonus4pc || "";
+          const relicImage = path.join(
+            "/set-images",
+            `${relicName.replace(/\s+/g, "_")}.png`,
+          );
+          return (
+            <RelicCardTemplate
+              key={index}
+              relicName={relicName}
+              relic2Pc={relic2Pc}
+              relic4Pc={relic4Pc}
+              relicImage={relicImage}
+            />
+          );
+        } else {
+          return null;
+        }
+      })}
+
+      <h4 className="text-3xl">Ornaments</h4>
+      {relics.map((relic, index) => {
+        if (relic.type !== "Relic Set") {
+          const ornamentName = relic.name;
+          const ornament2Pc = relic.bonus2pc;
+          const ornamentImage = path.join(
+            "/set-images",
+            `${ornamentName.replace(/\s+/g, "_")}.png`,
+          );
+          return (
+            <OrnamentCardTemplate
+              key={index}
+              ornamentName={ornamentName}
+              ornament2Pc={ornament2Pc}
+              ornamentImage={ornamentImage}
+            />
+          );
+        } else {
+          return null;
+        }
+      })}
+    </div>
+  );
 }
