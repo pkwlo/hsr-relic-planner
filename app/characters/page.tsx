a644e8e7a997ec20e015603f40f03d253951c75e
Patricia <pkwlo1992@gmail.com>
2024-05-27 16:16:19 -0700

    populate characters in character page

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -4,6 +4,48 @@ import React from "react";
 import Button from "@/components/Button";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
+import Image from "next/image";
+import characters from "./characters.json";
+
+function CharacterCardTemplate({
+  characterName,
+  characterImage,
+  characterType,
+  characterRarity,
+}: {
+  characterName: string;
+  characterImage: string;
+  characterType: string;
+  characterRarity: string;
+}) {
+  return (
+    <div className="flex flex-row">
+      <Image
+        src={characterImage}
+        alt={characterName}
+        width={160}
+        height={188}
+        style={{
+          width: 100,
+          height: 115,
+          border: "2px solid #FFFFFF",
+          borderRadius: 20,
+          margin: 10,
+          backgroundColor:
+            characterRarity === "rarity-5" ? "#FCFD96" : "#DCACD8",
+        }}
+        priority
+      />
+
+      <div className="flex flex-col py-4" style={{ width: 500 }}>
+        <h6 className="text-xl">{characterName}</h6>
+        <p className="mt-3">Element: {characterType}</p>
+        <p className="mt-3">Path: </p>
+        <p className="mt-3">Best Sets: </p>
+      </div>
+    </div>
+  );
+}
 
 export default function Page() {
   return (
@@ -12,16 +54,24 @@ export default function Page() {
       <main className="flex flex-row">
         <Sidebar />
         <div className="flex flex-col">
-          <div className="flex flex-row justify-center py-3">
-            <h3>Characters</h3>
-            <Button
-              onClick={function (): void {
-                throw new Error("Function not implemented.");
-              }}
-              text={"Add a Character"}
-            />
+          <h3 className="text-3xl">Characters</h3>
+          <div>
+            {characters.map((character, index) => {
+              const characterName = character.name;
+              const characterType = character.type;
+              const characterImage = character.local;
+              const characterRarity = character.rarity;
+              return (
+                <CharacterCardTemplate
+                  key={index}
+                  characterName={characterName}
+                  characterType={characterType}
+                  characterImage={characterImage}
+                  characterRarity={characterRarity}
+                />
+              );
+            })}
           </div>
-          <div>Placeholder section for character avatars.</div>
         </div>
       </main>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
 "use client";
 
-import Image from "next/image";
 import React from "react";
 import Button from "@/components/Button";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
 
 export default function Home() {
+  const [charPopup, setCharPopup] = React.useState<boolean>(false);
+  const [relicPopup, setRelicPopup] = React.useState<boolean>(false);
+
+  const addCharacter = (): void => {
+    setCharPopup(true);
+    setRelicPopup(false);
+  };
+
+  const addRelic = (): void => {
+    setRelicPopup(true);
+    setCharPopup(false);
+  };
+
+  const closePopup = (): void => {
+    setCharPopup(false);
+    setRelicPopup(false);
+  };
+
   return (
     <>
       <Header />
@@ -15,24 +32,44 @@ export default function Home() {
         <div className="flex flex-col">
           <div className="flex flex-row justify-center py-3">
             <h3>Characters</h3>
-            <Button
-              onClick={function (): void {
-                throw new Error("Function not implemented.");
-              }}
-              text={"Add a Character"}
-            />
+            <Button onClick={addCharacter} text={"Add a Character"} />
           </div>
-          <div>Placeholder for character avatars.</div>
+          <div>
+            {"You don't have any characters added. Start by adding some!"}
+          </div>
           <div className="flex flex-row justify-center py-3">
             <h3>Relics</h3>
-            <Button
-              onClick={function (): void {
-                throw new Error("Function not implemented.");
-              }}
-              text={"Add a Relic"}
-            />
+            <Button onClick={addRelic} text={"Add a Relic"} />
           </div>
-          <div>Placeholder for relic data.</div>
+          <div>{"You don't have any relics added. Start by adding some!"}</div>
+        </div>
+        <div className="flex flex-col">
+          {charPopup && (
+            <div className="popup">
+              <div className="popup-content">
+                <span className="close text-xl" onClick={closePopup}>
+                  &times;
+                </span>
+                {/* Content of your popup goes here */}
+                {/* For example: */}
+                <h2>Add Character</h2>
+                {/* Additional form fields or content */}
+              </div>
+            </div>
+          )}
+          {relicPopup && (
+            <div className="popup">
+              <div className="popup-content">
+                <span className="close text-xl" onClick={closePopup}>
+                  &times;
+                </span>
+                {/* Content of your popup goes here */}
+                {/* For example: */}
+                <h2>Add Relic</h2>
+                {/* Additional form fields or content */}
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </>
