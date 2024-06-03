5c88197d3329b3f7043db87b21de5f88be22e0f7
Patricia <pkwlo1992@gmail.com>
2024-06-03 11:22:37 -0700

    fix position of login and register button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Button from "./Button";
 
 const Header = () => {
   return (
-    <header>
-      <h1 className="text-3xl">HSR Relic Planner</h1>
+    <header className="flex flex-row justify-between items-center">
+      <h1 className="text-3xl" style={{ paddingLeft: "10px" }}>
+        HSR Relic Planner
+      </h1>
+      <div>
+        <Button text={"Log In"} onClick={() => goTo("/log-in")} />
+        <Button text={"Register"} onClick={() => goTo("/register")} />
+      </div>
     </header>
   );
 };
+
+function goTo(path: string) {
+  window.location.href = path;
+}
 
 export default Header;
