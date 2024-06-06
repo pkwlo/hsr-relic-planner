c5dff8a16ad34b64bd8b3f33f7c8be52efe8441a
Patricia <pkwlo1992@gmail.com>
2024-06-06 16:57:37 -0700

    add indivual constants for each main/substat per part + add save and clear button

--- components/AddRelic.tsx
+++ components/AddRelic.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import relics from "@/app/relic-sets/relics.json";
 import stats from "@/app/relic-sets/stats.json";
-import { useEffect } from "react";
 import styled from "@emotion/styled";
 
 const PartSelect = styled(Select)`
@@ -11,14 +10,35 @@ const PartSelect = styled(Select)`
   margin: 2px;
 `;
 
-const PartSelector = ({ part, stats }: { part: string; stats: any }) => {
+const PartSelector = ({
+  part,
+  stats,
+  mainS,
+  setMainS,
+  sub1,
+  setSub1,
+  sub2,
+  setSub2,
+  sub3,
+  setSub3,
+  sub4,
+  setSub4,
+}: {
+  part: string;
+  stats: any;
+  mainS: any;
+  setMainS: any;
+  sub1: any;
+  setSub1: any;
+  sub2: any;
+  setSub2: any;
+  sub3: any;
+  setSub3: any;
+  sub4: any;
+  setSub4: any;
+}) => {
   const main = getStatsByPart(part, "main", stats);
   const sub = getStatsByPart(part, "sub", stats);
-  const [mainS, setMainS] = useState(null);
-  const [sub1, setSub1] = useState(null);
-  const [sub2, setSub2] = useState(null);
-  const [sub3, setSub3] = useState(null);
-  const [sub4, setSub4] = useState(null);
 
   return (
     <div className="flex flex-col p-5">
@@ -28,9 +48,9 @@ const PartSelector = ({ part, stats }: { part: string; stats: any }) => {
         options={main}
         isClearable={true}
         isSearchable={true}
-        isDisabled={part === "hat" || part === "glove" ? true : false}
+        isDisabled={part === "hat" || part === "glove"}
         value={part === "hat" || part === "glove" ? main[0] : mainS}
-        onChange={(selectedOption: any) => setMainS(selectedOption)}
+        onChange={(selectedOption) => setMainS(selectedOption)}
       />
       {"Sub Stats"}
       <PartSelect
@@ -38,50 +58,45 @@ const PartSelector = ({ part, stats }: { part: string; stats: any }) => {
         isClearable={true}
         isSearchable={true}
         value={sub1}
-        onChange={(selectedOption: any) => setSub1(selectedOption)}
+        onChange={(selectedOption) => setSub1(selectedOption)}
       />
       <PartSelect
         options={sub}
         isClearable={true}
         isSearchable={true}
         value={sub2}
-        onChange={(selectedOption: any) => setSub2(selectedOption)}
+        onChange={(selectedOption) => setSub2(selectedOption)}
       />
       <PartSelect
         options={sub}
         isClearable={true}
         isSearchable={true}
         value={sub3}
-        onChange={(selectedOption: any) => setSub3(selectedOption)}
+        onChange={(selectedOption) => setSub3(selectedOption)}
       />
       <PartSelect
         options={sub}
         isClearable={true}
         isSearchable={true}
         value={sub4}
-        onChange={(selectedOption: any) => setSub4(selectedOption)}
+        onChange={(selectedOption) => setSub4(selectedOption)}
       />
     </div>
   );
 };
 
-interface ButtonProps {
-  onClick: () => void;
-  text: string;
-}
-
-const Button: React.FC<ButtonProps> = ({ onClick, text }) => {
+const Button = ({ onClick, text }: { onClick: () => void; text: string }) => {
   return (
     <button
       onClick={onClick}
       style={{
-        border: "2px solid #FFFFFF", // Border color
-        color: "#FFFFFF", // Text color
-        backgroundColor: "#7B67DF", // Background color
-        padding: "5px", // Padding
-        margin: "10px", // Margin
-        borderRadius: "5px", // Rounded corners
-        cursor: "pointer", // Pointer cursor on hover
+        border: "2px solid #FFFFFF",
+        color: "#FFFFFF",
+        backgroundColor: "#7B67DF",
+        padding: "5px",
+        margin: "10px",
+        borderRadius: "5px",
+        cursor: "pointer",
         width: "70px",
       }}
     >
@@ -109,60 +124,291 @@ function getStatsByPart(part: string, query: string, stats: any) {
 }
 
 const AddRelic = () => {
-  const [name, setName] = useState<string>("");
+  const [name, setName] = useState("");
+  const [hatStats, setHatStats] = useState({
+    mainS: null,
+    sub1: null,
+    sub2: null,
+    sub3: null,
+    sub4: null,
+  });
+  const [gloveStats, setGloveStats] = useState({
+    mainS: null,
+    sub1: null,
+    sub2: null,
+    sub3: null,
+    sub4: null,
+  });
+  const [shoesStats, setShoesStats] = useState({
+    mainS: null,
+    sub1: null,
+    sub2: null,
+    sub3: null,
+    sub4: null,
+  });
+  const [bodyStats, setBodyStats] = useState({
+    mainS: null,
+    sub1: null,
+    sub2: null,
+    sub3: null,
+    sub4: null,
+  });
+  const [sphereStats, setSphereStats] = useState({
+    mainS: null,
+    sub1: null,
+    sub2: null,
+    sub3: null,
+    sub4: null,
+  });
+  const [ropeStats, setRopeStats] = useState({
+    mainS: null,
+    sub1: null,
+    sub2: null,
+    sub3: null,
+    sub4: null,
+  });
 
   useEffect(() => {
-    // setName("The Ashblazing Grand Duke");
     setName("Space Sealing Station");
   }, []);
 
-  // if no relic is selected - default selection screen
+  const clear = () => {
+    setHatStats({
+      mainS: null,
+      sub1: null,
+      sub2: null,
+      sub3: null,
+      sub4: null,
+    });
+    setGloveStats({
+      mainS: null,
+      sub1: null,
+      sub2: null,
+      sub3: null,
+      sub4: null,
+    });
+    setShoesStats({
+      mainS: null,
+      sub1: null,
+      sub2: null,
+      sub3: null,
+      sub4: null,
+    });
+    setBodyStats({
+      mainS: null,
+      sub1: null,
+      sub2: null,
+      sub3: null,
+      sub4: null,
+    });
+    setSphereStats({
+      mainS: null,
+      sub1: null,
+      sub2: null,
+      sub3: null,
+      sub4: null,
+    });
+    setRopeStats({
+      mainS: null,
+      sub1: null,
+      sub2: null,
+      sub3: null,
+      sub4: null,
+    });
+  };
+
+  const save = () => {
+    console.log("Hat Stats:", hatStats);
+    console.log("Glove Stats:", gloveStats);
+    console.log("Shoes Stats:", shoesStats);
+    console.log("Body Stats:", bodyStats);
+    console.log("Sphere Stats:", sphereStats);
+    console.log("Rope Stats:", ropeStats);
+  };
+
   if (name === "") {
     return (
       <div className="p-2 border">
         <h1 className="text-3xl">Select a set to add</h1>
-        {
-          // all the sets here to be selected relics.map...
-        }
+        {/* Render the options for selecting a relic set */}
       </div>
     );
-  }
-
-  // if a relic is selected
-  else {
+  } else {
     const type = getTypeByName(name, relics);
 
     if (type === "Relic Set") {
       return (
-        <>
-          <div className="flex flex-col">
-            <div className="flex flex-row m-3 items-center">
-              <Button text={"< Back"} onClick={() => setName("")} />
-              <div className="text-3xl">{name}</div>
-            </div>
-            <div className="flex flex-row">
-              <PartSelector part={"hat"} stats={stats} />
-              <PartSelector part={"glove"} stats={stats} />
-              <PartSelector part={"shoes"} stats={stats} />
-              <PartSelector part={"body"} stats={stats} />
-            </div>
+        <div className="flex flex-col">
+          <div className="flex flex-row m-3 items-center">
+            <Button text={"< Back"} onClick={() => setName("")} />
+            <div className="text-3xl">{name}</div>
           </div>
-        </>
+          <div className="flex flex-row">
+            <PartSelector
+              part={"hat"}
+              stats={stats}
+              mainS={hatStats.mainS}
+              setMainS={(val: any) =>
+                setHatStats((prev) => ({ ...prev, mainS: val }))
+              }
+              sub1={hatStats.sub1}
+              setSub1={(val: any) =>
+                setHatStats((prev) => ({ ...prev, sub1: val }))
+              }
+              sub2={hatStats.sub2}
+              setSub2={(val: any) =>
+                setHatStats((prev) => ({ ...prev, sub2: val }))
+              }
+              sub3={hatStats.sub3}
+              setSub3={(val: any) =>
+                setHatStats((prev) => ({ ...prev, sub3: val }))
+              }
+              sub4={hatStats.sub4}
+              setSub4={(val: any) =>
+                setHatStats((prev) => ({ ...prev, sub4: val }))
+              }
+            />
+            <PartSelector
+              part={"glove"}
+              stats={stats}
+              mainS={gloveStats.mainS}
+              setMainS={(val: any) =>
+                setGloveStats((prev) => ({ ...prev, mainS: val }))
+              }
+              sub1={gloveStats.sub1}
+              setSub1={(val: any) =>
+                setGloveStats((prev) => ({ ...prev, sub1: val }))
+              }
+              sub2={gloveStats.sub2}
+              setSub2={(val: any) =>
+                setGloveStats((prev) => ({ ...prev, sub2: val }))
+              }
+              sub3={gloveStats.sub3}
+              setSub3={(val: any) =>
+                setGloveStats((prev) => ({ ...prev, sub3: val }))
+              }
+              sub4={gloveStats.sub4}
+              setSub4={(val: any) =>
+                setGloveStats((prev) => ({ ...prev, sub4: val }))
+              }
+            />
+            <PartSelector
+              part={"shoes"}
+              stats={stats}
+              mainS={shoesStats.mainS}
+              setMainS={(val: any) =>
+                setShoesStats((prev) => ({ ...prev, mainS: val }))
+              }
+              sub1={shoesStats.sub1}
+              setSub1={(val: any) =>
+                setShoesStats((prev) => ({ ...prev, sub1: val }))
+              }
+              sub2={shoesStats.sub2}
+              setSub2={(val: any) =>
+                setShoesStats((prev) => ({ ...prev, sub2: val }))
+              }
+              sub3={shoesStats.sub3}
+              setSub3={(val: any) =>
+                setShoesStats((prev) => ({ ...prev, sub3: val }))
+              }
+              sub4={shoesStats.sub4}
+              setSub4={(val: any) =>
+                setShoesStats((prev) => ({ ...prev, sub4: val }))
+              }
+            />
+            <PartSelector
+              part={"body"}
+              stats={stats}
+              mainS={bodyStats.mainS}
+              setMainS={(val: any) =>
+                setBodyStats((prev) => ({ ...prev, mainS: val }))
+              }
+              sub1={bodyStats.sub1}
+              setSub1={(val: any) =>
+                setBodyStats((prev) => ({ ...prev, sub1: val }))
+              }
+              sub2={bodyStats.sub2}
+              setSub2={(val: any) =>
+                setBodyStats((prev) => ({ ...prev, sub2: val }))
+              }
+              sub3={bodyStats.sub3}
+              setSub3={(val: any) =>
+                setBodyStats((prev) => ({ ...prev, sub3: val }))
+              }
+              sub4={bodyStats.sub4}
+              setSub4={(val: any) =>
+                setBodyStats((prev) => ({ ...prev, sub4: val }))
+              }
+            />
+          </div>
+          <div className="flex flex-row">
+            <Button text={"Clear"} onClick={clear} />
+            <Button text={"Save"} onClick={save} />
+          </div>
+        </div>
       );
     } else {
       return (
-        <>
-          <div className="flex flex-col">
-            <div className="flex flex-row m-3 items-center">
-              <Button text={"< Back"} onClick={() => setName("")} />
-              <div className="text-3xl">{name}</div>
-            </div>
-            <div className="flex flex-row">
-              <PartSelector part={"sphere"} stats={stats} />
-              <PartSelector part={"rope"} stats={stats} />
-            </div>
+        <div className="flex flex-col">
+          <div className="flex flex-row m-3 items-center">
+            <Button text={"< Back"} onClick={() => setName("")} />
+            <div className="text-3xl">{name}</div>
           </div>
-        </>
+          <div className="flex flex-row">
+            <PartSelector
+              part={"sphere"}
+              stats={stats}
+              mainS={sphereStats.mainS}
+              setMainS={(val: any) =>
+                setSphereStats((prev) => ({ ...prev, mainS: val }))
+              }
+              sub1={sphereStats.sub1}
+              setSub1={(val: any) =>
+                setSphereStats((prev) => ({ ...prev, sub1: val }))
+              }
+              sub2={sphereStats.sub2}
+              setSub2={(val: any) =>
+                setSphereStats((prev) => ({ ...prev, sub2: val }))
+              }
+              sub3={sphereStats.sub3}
+              setSub3={(val: any) =>
+                setSphereStats((prev) => ({ ...prev, sub3: val }))
+              }
+              sub4={sphereStats.sub4}
+              setSub4={(val: any) =>
+                setSphereStats((prev) => ({ ...prev, sub4: val }))
+              }
+            />
+            <PartSelector
+              part={"rope"}
+              stats={stats}
+              mainS={ropeStats.mainS}
+              setMainS={(val: any) =>
+                setRopeStats((prev) => ({ ...prev, mainS: val }))
+              }
+              sub1={ropeStats.sub1}
+              setSub1={(val: any) =>
+                setRopeStats((prev) => ({ ...prev, sub1: val }))
+              }
+              sub2={ropeStats.sub2}
+              setSub2={(val: any) =>
+                setRopeStats((prev) => ({ ...prev, sub2: val }))
+              }
+              sub3={ropeStats.sub3}
+              setSub3={(val: any) =>
+                setRopeStats((prev) => ({ ...prev, sub3: val }))
+              }
+              sub4={ropeStats.sub4}
+              setSub4={(val: any) =>
+                setRopeStats((prev) => ({ ...prev, sub4: val }))
+              }
+            />
+          </div>
+          <div className="flex flex-row">
+            <Button text={"Clear"} onClick={clear} />
+            <Button text={"Save"} onClick={save} />
+          </div>
+        </div>
       );
     }
   }
