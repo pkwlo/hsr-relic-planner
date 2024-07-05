9e200d87b5ed7c67934465e53c605318964c5ff5
Patricia <pkwlo1992@gmail.com>
2024-07-05 13:03:39 -0700

    fix vercel issue

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
         <Sidebar />
         <div className="flex flex-col p-3 px-6">
           <h3 className="text-3xl mb-3">Testing grounds</h3>
-          <AddRelic charSelected={""} />
         </div>
       </main>
     </>
