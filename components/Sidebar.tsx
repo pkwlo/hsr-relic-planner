2a8011b8e4368581baf0132ab4defcc86ca6f9b3
Patricia <pkwlo1992@gmail.com>
2024-07-15 16:52:56 -0700

    add copywright info

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,8 +3,11 @@ import SideButton from "./SideButton";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar" style={{ width: "230px", minWidth: "230px" }}>
-      <div className="flex flex-col">
+    <div
+      className="sidebar flex-col"
+      style={{ width: "230px", minWidth: "230px" }}
+    >
+      <div className="flex flex-col" style={{ minHeight: "80vh" }}>
         <SideButton onClick={() => goTo("/")} text={"Home"} />
         <SideButton
           onClick={() => goTo("/relic-dashboard")}
@@ -16,6 +19,12 @@ const Sidebar = () => {
           text={"Relic & Ornament Sets"}
         />
         <SideButton onClick={() => goTo("/report")} text={"Report a Bug"} />
+      </div>
+      <div style={{ marginTop: "auto", textAlign: "center" }}>
+        <p className="text-s text-center">Created by Nori, 2024</p>
+        <p className="text-xs text-center">
+          Honkai Star Rail © MiHoYo Co., Ltd.
+        </p>
       </div>
     </div>
   );
